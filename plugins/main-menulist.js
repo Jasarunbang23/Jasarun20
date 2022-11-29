c7e639504428ca02f3801fd9fe5c9ddf2c2b75f1
Jasarunbang23 <119160730+Jasarunbang23@users.noreply.github.com>
2022-11-29 08:48:25 +0700

    Update main-menulist.js

--- plugins/main-menulist.js
+++ plugins/main-menulist.js
@@ -483,12 +483,12 @@ ptt: false, seconds: 0,contextInfo: {
     description: sgc,
     title: "Kᴛɪᴋ .sᴏᴜɴᴅᴍᴇɴᴜ ᴜɴᴛᴋ ʏɢ ʟᴀɪɴ",
     body: wm,
-    thumbnail: await (await fetch('https://telegra.ph/file/8e6ec74578b587a4efe55.jpg')).buffer(),
+    thumbnail: await (await fetch('https://telegra.ph/file/2a6539175e20212c9954f.jpg')).buffer(),
     sourceUrl: sgc
  	  /*   sourceUrl: sig,
            title: '◄⟬ ●━━━ ⧏ ⧎ ⧐ ━━━● ⟭►',  
             body: 'Now Playing...', 
-           thumbnail: await (await fetch('https://telegra.ph/file/8e6ec74578b587a4efe55.jpg')).buffer()*/
+           thumbnail: await (await fetch('https://telegra.ph/file/2a6539175e20212c9954f.jpg')).buffer()*/
 }
      }
     })
